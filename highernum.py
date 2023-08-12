1bc83fc5acb2036650c96d2347447a469484883a
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-08-12 21:02:27 +1000

    Create highernum.py

---
+++ highernum.py
@@ -0,0 +1,9 @@
+print("Pick 2 random numbers between 1 and 100.")
+print("And I will tell which one is higher.")
+fnum = int(input('Pick your first number: '))
+snum = int(input('Pick your second number: '))
+
+if fnum > snum:
+    print(fnum , "is higher.")
+else:
+    print(snum, "is higher.")
