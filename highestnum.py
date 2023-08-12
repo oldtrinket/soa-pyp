f0286287d662c6459a64551cd2f413145c35b49d
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-08-12 21:04:24 +1000

    Update highestnum.py

--- highestnum.py
+++ highestnum.py
@@ -1,4 +1,7 @@
-
+#In this code, I first initialize the variable highest to the value of fnum.
+#Then, I use the if statements to check if snum and tnum are greater than highest. If they are,
+#then I update the value of highest to the new value. Finally, I print the value of highest.
+#This code will now only print the highest number among the three random numbers
 
 print("Pick 3 random numbers between 1 and 100.")
 print("And I will tell which one is the highest.")
