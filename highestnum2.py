7d1510cea2ed15c5f46655865343f0458390bd27
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-08-16 18:12:57 +1000

    Update highestnum2.py

--- highestnum2.py
+++ highestnum2.py
@@ -1,1 +1,11 @@
+def find_highest_number(num1, num2, num3):
+  """Finds and returns the highest number among three numbers."""
+  return max(num1, num2, num3)
 
+def main():
+  """Generates three random numbers between 1 and 100 and prints the highest one."""
+  num1 = random.randint(1, 100)
+  num2 = random.randint(1, 100)
+  num3 = random.randint(1, 100)
+
+ 
