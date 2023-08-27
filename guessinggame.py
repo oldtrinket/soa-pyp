cabb08c4c2bc73141fe7fa6e962b64e73092e959
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-08-27 18:52:17 +1000

    Guessing Game v4

---
+++ guessinggame.py
@@ -0,0 +1,44 @@
+#HENRY GERVACIO 880633837
+#Guessing Game
+
+import random
+
+while True:
+
+    random_number = random.randint(1, 100)
+    max_attempts = 5
+    guesses = []
+
+    for attempt in range(1, max_attempts + 1):
+
+        guess = input("Enter your guess (1-100) or press 'q' to quit [Attempt {}/{}]: ".format(attempt, max_attempts))
+
+        if guess.lower() == 'q':
+            print("Quitting the game. The number was {}.".format(random_number))
+            break
+
+        if not guess.isdigit():
+            print("Please enter a valid number.")
+            continue
+
+        guess = int(guess)
+        guesses.append(guess)
+
+        if guess == random_number:
+            print("Congratulations! You've guessed the correct number.")
+            print("Your guesses were: {}".format(guesses))
+            break
+        elif guess < random_number:
+            print("Too low. Try again.")
+        else:
+            print("Too high. Try again.")
+
+    else:
+        print("Sorry, you've reached the maximum number of attempts. The correct number was {}.".format(random_number))
+        print("Your guesses were: {}".format(guesses))
+
+
+    replay = input("Do you want to play again? (y/n): ").lower()
+    if replay != 'y':
+        print("Thanks for playing! Goodbye.")
+        break
