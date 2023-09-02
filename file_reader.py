dd4ce94d3cbe6d493b8ea8d7b5cbeb8337d2f71e
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-09-02 22:36:29 +1000

    Create file_reader.py
    needs userfile.txt to work

---
+++ file_reader.py
@@ -0,0 +1,12 @@
+file_path = "userfile.txt"
+
+user_response = input("Do you want to print the content of the text file? (yes/no) ").lower()
+
+if user_response == 'yes':
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+        for line in data:
+            name, surname, password = line.strip().split()  # Assuming the data in the file is space-separated
+            print(f"Net User: {name}.{surname} Password: {password}")
+else:
+    print("Okay, content will not be printed.")
