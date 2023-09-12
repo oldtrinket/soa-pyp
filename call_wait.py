29188d7bde012f114a2e6ed6985ccdedf1f74d66
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-09-12 22:02:33 +1000

    Update call_wait.py

--- call_wait.py
+++ call_wait.py
@@ -1,6 +1,6 @@
 # get the current time 
 # then use a while loop to wait 5 seconds 
 # use the while loop to count down each second eg 5 4 3 2 1 0 
-#every second print the following string  
+# every second print the following string  
 #“Your call is important to us. We don’t want to hire anyone else to answer your call, so please hold for 3 seconds” 
 #Do not use a wait function – use the now() function 
