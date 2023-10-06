a4ed97bdcb513762c961b2853a6f00b4d9d97a87
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-06 21:51:49 +1100

    Create generate_password.py

---
+++ generate_password.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length=8):
+    if length < 6:
+        print("Password length should be at least 6 characters.")
+        return None
+    
+    charset = string.ascii_letters + string.digits + "!@#$%^"
+    password = [
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice("!@#$%^"),
+    ] + [random.choice(charset) for i in range(length-4)]
+    
+    random.shuffle(password)
+    
+    return ''.join(password)
+
+# Test
+print(generate_password(10))  # This will print a 10-character password that meets the standards
