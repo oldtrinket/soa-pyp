407043e214dd63a1a6b584dbf4cf212308c9d2c2
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-06 21:52:33 +1100

    Create save_user.py

---
+++ save_user.py
@@ -0,0 +1,3 @@
+def save_user(username, password, security_question, security_answer):
+    with open("accounts.txt", "a") as file:
+        file.write(f"{username}:{password}:{security_question}:{security_answer}\n")
