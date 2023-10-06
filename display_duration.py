78fbb7d7dcf7e8f6cc50736a5954679e4635d629
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-06 22:00:11 +1100

    Create display_duration.py

---
+++ display_duration.py
@@ -0,0 +1,11 @@
+def display_duration():
+    end_time = datetime.now()
+    duration = end_time - start_time
+    minutes, seconds = divmod(duration.seconds, 60)
+    print(f"You've used the system for {minutes} minutes and {seconds} seconds.")
+    time.sleep(2)  # delay for 2 seconds
+
+# In your main_menu function:
+elif choice == '6' or choice == 'exit':
+    display_duration()
+    break
