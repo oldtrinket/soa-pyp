c88a89c9eac5c451d719a0c2b117cf42164e6880
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-11 20:03:53 +1100

    Update is_admin_authenticated.py

--- is_admin_authenticated.py
+++ is_admin_authenticated.py
@@ -1,2 +1,2 @@
 def is_admin_authenticated(username, password):
-  return username == SYSTEM_ADMIN_USERNAME and password == SYSTEM_ADMIN_
+  return username == SYSTEM_ADMIN_USERNAME and password == SYSTEM_ADMIN_PASSWORD
