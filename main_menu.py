1daa64cb2929d6c6a4d5e8ee63ee08752db7baab
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-23 21:29:53 +1100

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -10,7 +10,7 @@ def main_menu():
         choice = input("\nChoose an option: ").lower()
 
         if choice == '1' or choice == 'login':
-            # Call login function
+        
       
        
            
