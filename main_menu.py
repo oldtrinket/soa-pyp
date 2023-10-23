dd5de531dba80f6a6dc5e8662384b45d2b44516d
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-23 21:30:44 +1100

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -5,8 +5,7 @@ def main_menu():
         print("2. Register")
         print("3. Change Password")
         print("4. Reset Password")
-        print("5. View Accounts (Admin Only)")
-       
+      
        
 
      
