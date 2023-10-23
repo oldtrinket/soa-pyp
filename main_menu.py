94a4f8dbf3b6d16f750273b5c1f1f4b232346199
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-23 21:27:23 +1100

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -23,7 +23,7 @@ def main_menu():
             pass
         elif choice == '5' or choice == 'view accounts':
             # Call view accounts function
-            pass
+     
       
            
    
