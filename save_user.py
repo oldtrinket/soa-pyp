410b4fafc482be6111ca7385a6df042eea18cf9b
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-31 20:21:34 +1100

    Update save_user.py

--- save_user.py
+++ save_user.py
@@ -1,3 +1,5 @@
-
+def save_user(username, password, security_question, security_answer):
+    with open("accounts.txt", "a") as file:
+        file.write(f"{username}:{password}:{security_question}:{security_answer}\n")
     
         
